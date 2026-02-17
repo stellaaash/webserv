fdbb4a838b96acc9b0d05b509891ee455992be10
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-02-17 13:21:48 +0000

    ConMan: Add draft of Response interface

--- include/Response.hpp
+++ include/Response.hpp
@@ -1,9 +1,21 @@
 #ifndef RESPONSE_HPP
 #define RESPONSE_HPP
 
-class Response {
+#include <string>
+
+#include "HTTP_Message.hpp"
+#include "config.hpp"
+
+class Response : virtual public HTTP_Message {
 public:
+    Response();
+    ~Response();
+
+    std::string serialize() const;
+
 private:
+    HTTP_Code   _code;
+    std::string _response_string;
 };
 
 #endif
