43e1abb270bb49499ed688c0f400a457f926a027
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-02-17 09:26:30 +0000

    ConMan: Add first draft of Connection class

--- include/Connection.hpp
+++ include/Connection.hpp
@@ -1,9 +1,25 @@
 #ifndef CONNECTION_HPP
 #define CONNECTION_HPP
 
+#include "Request.hpp"
+#include "Response.hpp"
+#include "config.hpp"
+
 class Connection {
-    private:
-    public:
+public:
+    Connection(const Config_Server& config);
+    ~Connection();
+
+    const Response& response() const;
+    const Request&  request() const;
+
+private:
+    Request              _request;
+    Response             _response;
+    const Config_Server& _config;
+
+    // TODO Socket
+    // TODO Read buffer? How?
 };
 
 #endif
