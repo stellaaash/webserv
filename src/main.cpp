fc77f705457b766a593f9ff4aecdb0ddc430f5fc
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-03-03 13:07:13 +0000

    Parsing: Try the config lexer out in the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+#include <fstream>
+#include <vector>
+
+#include "ConfigLexer.hpp"
 #include "config.hpp"
 
 int main() {
     Config config = mock_config();
+
+    std::ifstream config_file("./config/1.conf");
+
+    std::vector<ConfigParser::Lexer::Token> tokens = ConfigParser::Lexer::lexConfig(config_file);
     return 0;
 }
