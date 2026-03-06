caf4497fff49698171e9f190cd4073d6b3340ee0
0yech <Nelson.rey02@gmail.com>
2026-03-06 02:39:43 +0100

    Running server on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include <cerrno>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+
+#include "ConnectionManager.hpp"
+#include "Listener.hpp"
 #include "config.hpp"
+#include "socket_utils.hpp"
 
 int main() {
-    Config config = mock_config();
+    Config cfg = mock_config();
+
+    int listen_fd = make_listen_socket(cfg.server);
+    if (listen_fd < 0) {
+        std::perror("make_listen_socket");
+        return 1;
+    }
+
+    ConnectionManager manager;
+    manager.add(new Listener(&cfg.server, listen_fd));
+    std::cout << "[MAIN] Server running : http://127.0.0.1:8080/" << std::endl;
+    std::cout << "[MAIN] Listening on fd=" << listen_fd << std::endl;
+    manager.run();
+
     return 0;
 }
