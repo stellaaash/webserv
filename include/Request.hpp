8326a9471e61ff421af12fb0443f5d0072a8dfde
0yech <Nelson.rey02@gmail.com>
2026-03-20 00:28:23 +0100

    Added new members/getters/setters to Request object

--- include/Request.hpp
+++ include/Request.hpp
@@ -1,6 +1,9 @@
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
+#include <cstddef>
+#include <string>
+
 #include "HTTP_Message.hpp"
 #include "config.hpp"
 
@@ -18,16 +21,22 @@ public:
     const Request& operator=(const Request&);
     ~Request();
 
-    HTTP_Method    method() const;
-    Status_Parsing status() const;
+    HTTP_Method        method() const;
+    Status_Parsing     status() const;
+    const std::string& target() const;
+    size_t             content_length() const;
 
     void set_config(const Config_Location* const);
     void set_method(HTTP_Method);
     void set_status(Status_Parsing);
+    void set_target(const std::string&);
+    void set_content_length(size_t);
 
 private:
     const Config_Location* _config;
 
+    std::string    _target;
+    size_t         _content_length;
     HTTP_Method    _method;
     Status_Parsing _status;
 };
--- src/ConnectionManager/Request.cpp
+++ src/ConnectionManager/Request.cpp
@@ -5,20 +5,23 @@
 #include "HTTP_Message.hpp"
 #include "config.hpp"
 
-Request::Request() : _config(NULL), _method(UNDEFINED), _status(EMPTY) {}
+Request::Request() : _config(NULL), _target(""), _content_length(0), _method(UNDEFINED), _status(EMPTY) {}
 
 Request::Request(const Config_Location* const config, HTTP_Method method)
-    : _config(config), _method(method), _status(EMPTY) {
+    : _config(config), _target(""), _content_length(0), _method(method), _status(EMPTY) {
     assert(config && "Config_Location pointer");
 }
 
 Request::Request(const Request& other)
-    : HTTP_Message(other), _config(other._config), _method(other._method), _status(other._status) {}
+    : HTTP_Message(other), _config(other._config), _target(other._target), _content_length(other._content_length), _method(other._method), _status(other._status) {}
 
 const Request& Request::operator=(const Request& other) {
     if (this == &other) return *this;
 
+    HTTP_Message::operator=(other);
     _config = other._config;
+    _target = other._target;
+    _content_length = other._content_length;
     _method = other._method;
     _status = other._status;
 
@@ -35,6 +38,14 @@ Status_Parsing Request::status() const {
     return _status;
 }
 
+const std::string& Request::target() const {
+    return _target;
+}
+
+size_t Request::content_length() const {
+    return _content_length;
+}
+
 void Request::set_config(const Config_Location* const config) {
     assert(config && "Config_Server pointer");
 
@@ -48,4 +59,12 @@ void Request::set_method(HTTP_Method method) {
 void Request::set_status(Status_Parsing status) {
     assert(status > _status && "Walking back status");  // Status should only progress, not go back
     _status = status;
+}
+
+void Request::set_target(const std::string& target) {
+    _target = target;
+}
+
+void Request::set_content_length(size_t content_length) {
+    _content_length = content_length;
 }
