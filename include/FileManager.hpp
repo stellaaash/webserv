35a802940c616a2a171d4eb1ea714c6ce6df9059
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-03-20 13:50:59 +0000

    FileMan: Add function to create a file

--- include/FileManager.hpp
+++ include/FileManager.hpp
@@ -3,7 +3,9 @@
 
 #include "config.hpp"
 
-bool is_regular_file(const File_Path& path);
+bool is_regular_file(const File_Path&);
 int  fetch_file(const File_Path&);
+
+int create_file(const File_Path&);
 
 #endif  // FILEMANAGER_HPP
