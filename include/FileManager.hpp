553028f03e1a645592a5758439ba391b3fcc14de
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-03-23 08:32:56 +0000

    FileMan: Create append_file function
    This will be useful when getting post requests to store files; we can
    just use this function to write a string to a file descriptor chunk by
    chunk.

--- include/FileManager.hpp
+++ include/FileManager.hpp
@@ -1,6 +1,8 @@
 #ifndef FILEMANAGER_HPP
 #define FILEMANAGER_HPP
 
+#include <sys/types.h>
+
 #include <string>
 
 typedef std::string File_Path;
@@ -10,6 +12,7 @@ bool      is_directory(const File_Path&);
 int       fetch_file(const File_Path&);
 File_Path standardize_path(const std::string& path);
 
-int create_file(const File_Path&);
+int     create_file(const File_Path&);
+ssize_t append_file(int, const std::string&);
 
 #endif  // FILEMANAGER_HPP
--- src/FileManager/files_write.cpp
+++ src/FileManager/files_write.cpp
@@ -1,8 +1,14 @@
 #include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#include <algorithm>
 #include <cstdio>
 
 #include "FileManager.hpp"
+
+// Size of buffer to be used when writing data to files
+const size_t write_size = 1024;
 
 /**
  * @brief Creates a file in read-write mode, and returns its file descriptor.
@@ -16,4 +22,26 @@ int create_file(const File_Path& path) {
     }
 
     return fd;
+}
+
+/**
+ * @brief Appends a string's data to a file through its file descriptor.
+ * Note that errors don't guarantee that the file wasn't written to.
+ *
+ * @return The size written, or -1 if an error occurred.
+ */
+ssize_t append_file(int fd, const std::string& data) {
+    std::string::size_type write_index = 0;
+
+    while (write_index < data.length()) {
+        std::string::size_type chunk_size = std::min(write_size, data.length() - write_index);
+        ssize_t                status = write(fd, data.c_str() + write_index, chunk_size);
+        if (status < 0) {
+            return -1;
+        }
+
+        write_index += static_cast<std::string::size_type>(status);
+    }
+
+    return static_cast<ssize_t>(write_index);
 }
