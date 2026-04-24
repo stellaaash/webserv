b0a498d8a7b4b4d1766682eba6e34cfec2f1dcc7
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-04-24 14:55:08 +0000

    RePro: Add resource path resolution and refactor processing logic
    Get requests are starting to work pretty well! Next is the reading from
    file descriptors.

--- src/RequestProcessor/request_processor.cpp
+++ src/RequestProcessor/request_processor.cpp
@@ -8,46 +8,53 @@
 #include "file_manager.hpp"
 
 ResponseStatus Connection::process_get_request(const FilePath& resource_path) {
-    // Fetch the resource or generate content
-    if (is_directory(resource_path) == true) {
-        if (_request.config().autoindex == true) {
-            _response.append_body(create_listing(resource_path));
+    FilePath path = resource_path;
+
+    if (is_directory(path) == true) {
+        if (_request.config().index.empty() == false) {
+            // Resource to fetch becomes the index file, not the folder itself
+            path = _request.config().index;
+        } else if (_request.config().autoindex == true) {
+            _response.append_body(create_listing(path));
             _response.set_code(200);
             _response.set_response_string("OK");
         } else {
             _response = error_response(404);
         }
-        return _response.status();
-    } else if (!is_regular_file(resource_path)) {
+    } else if (!is_regular_file(path)) {  // We don't serve anything other than files or directories
         _response = error_response(404);
-        return _response.status();
     }
 
-    int fd = fetch_file(resource_path);
-    if (fd < 0) {
-        Logger(LOG_ERROR) << "[process_request] - fetch_file" << strerror(errno);
-        if (errno == EACCES)
-            _response = error_response(403);
-        else if (errno == ENOENT)
-            _response = error_response(404);
-        else if (errno == EINVAL || errno == ENAMETOOLONG) {
-            _response = error_response(400);
-        } else
-            _response = error_response(500);
-        return _response.status();
+    // Fetch file on disk
+    if (_response.status() != RES_ERROR && _response.body().empty() == true) {
+        int fd = fetch_file(path);
+        if (fd < 0) {
+            Logger(LOG_ERROR) << "[process_request] - fetch_file" << strerror(errno);
+            if (errno == EACCES)
+                _response = error_response(403);
+            else if (errno == ENOENT)
+                _response = error_response(404);
+            else if (errno == EINVAL || errno == ENAMETOOLONG) {
+                _response = error_response(400);
+            } else
+                _response = error_response(500);
+        } else {
+            // _response.set_fd(fd); // TODO Temporary, will be added once we can read from fds
+            _response.append_body("Yayyyyyy!!");
+            _response.set_code(200);
+            _response.set_response_string("OK");
+        }
     }
 
-    // _response.set_fd(fd);
-    _response.append_body("Hewwo :3");  // TODO Temporary, need to implement reading from fds
-    _response.set_code(200);
-    _response.set_response_string("OK");
     std::stringstream stream;
     if (_response.body().empty() == false) {
         stream << _response.body().size();
     } else if (_response.fd() >= 0) {
-        stream << file_length(resource_path);
+        stream << file_length(path);
     } else {
-        stream << 0;  // If no body is present and no file is to be read, the body is empty
+        Logger(LOG_ERROR) << "[process_get_request] - Impossible state! Response has no body and "
+                             "no fd to read from";
+        stream << 0;
     }
     _response.set_header("Content-Length", stream.str());
     // TODO Set headers, MIME types??
@@ -56,9 +63,10 @@ ResponseStatus Connection::process_get_request(const FilePath& resource_path) {
 }
 
 ResponseStatus Connection::process_request() {
-    // TODO Isolate the resource path we need (need the fileman pr merged first)
-    // This is hardcoded for now until I can get the real resource path
-    FilePath resource_path = "html/index.html";
+    std::string relative_path = _request.target().substr(_request.config().name.length());
+    if (relative_path[0] == '/')  // If we still have a slash at the beginning, remove it
+        relative_path.erase(0, relative_path.find_first_not_of('/'));
+    FilePath resource_path = resolve_path(relative_path, _request.config().root);
 
     switch (_request.method()) {
         case GET:
