7f270af79c6fd024c9b7136e0439295b4aed3d1c
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-05-06 12:47:35 +0200

    RePro: 404 error when trying to delete a resource that doesn't exist

--- src/RequestProcessor/delete.cpp
+++ src/RequestProcessor/delete.cpp
@@ -5,17 +5,16 @@
 #include "file_manager.hpp"
 
 void Connection::process_delete_request(const FilePath& resource_path) {
-    // TODO Forbidden function?? Hello?? I'm so confused right now
-    // This is the only way to remove a file, and guess what? It's not in the allowed functions list
-    // This makes me rethink what the allowed functions list actually means, whether we can use
-    // other syscalls like rename, getcwd etc
-    if (remove(resource_path.c_str()) < 0) {
+    if (is_regular_file(resource_path) == false && is_directory(resource_path) == false) {
+        _response = error_response(404, false);
+        return;
+    }
+    if (std::remove(resource_path.c_str()) < 0) {
         // TODO check the exact errno values, and create a better error response
         _response = error_response(500, false);
         return;
     }
-    _response.set_code(200);
-    _response.set_response_string("OK");
-    _response.set_header("Content-Length", "0");
+    _response.set_code(204);
+    _response.set_response_string("No Content");
     return;
 }
