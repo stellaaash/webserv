12f9e1e839f8abde1cb129697940c3de7492cf51
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-05-06 13:45:22 +0200

    RePro: Add a few more possible returned error messages to DELETE

--- src/RequestProcessor/delete.cpp
+++ src/RequestProcessor/delete.cpp
@@ -1,3 +1,4 @@
+#include <cerrno>
 #include <cstdio>
 
 #include "Connection.hpp"
@@ -5,13 +6,18 @@
 #include "file_manager.hpp"
 
 void Connection::process_delete_request(const FilePath& resource_path) {
-    if (is_regular_file(resource_path) == false && is_directory(resource_path) == false) {
-        _response = error_response(404, false);
-        return;
-    }
     if (std::remove(resource_path.c_str()) < 0) {
-        // TODO check the exact errno values, and create a better error response
-        _response = error_response(500, false);
+        if (errno == ENOENT) {
+            _response = error_response(404, false);
+        } else if (errno == EACCES || errno == EPERM) {
+            _response = error_response(403, false);
+        } else if (errno == EINVAL) {
+            _response = error_response(400, false);
+        } else if (errno == EBUSY) {
+            _response = error_response(503, false);
+        } else {
+            _response = error_response(500, false);
+        }
         return;
     }
     _response.set_code(204);
