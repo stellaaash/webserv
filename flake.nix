e664ab0d47e9325cfd56ac12d18dbb76dd87c84f
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-05-08 08:00:04 +0200

    Meta: add nix flake with direnv setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based C++ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+	gnumake
+	clang
+	valgrind
+      ];
+
+      shellHook = ''
+	echo "Time to web and serv."
+      '';
+    };
+  };
+}
