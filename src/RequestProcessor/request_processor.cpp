ea18b54b845e825c7b735ddf324655497f4cb4e5
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-05-13 08:10:03 +0000

    RePro: When redirecting, add a Location header
    This header will point to the resource in the redirected location

--- src/RequestProcessor/request_processor.cpp
+++ src/RequestProcessor/request_processor.cpp
@@ -9,21 +9,26 @@
 #include "config.hpp"
 
 void Connection::process_request() {
+    std::string relative_path = _request.target().substr(_request.config()->name.length());
+    if (!relative_path.empty() &&  // If we still have a slash at the beginning, remove it
+        relative_path[0] == '/')
+        relative_path.erase(0, relative_path.find_first_not_of('/'));
+
     // TODO Redirection checking should probably happen before the body arrives
     if (_request.config()->redirect.first >= 300 && _request.config()->redirect.first <= 399) {
         _response.set_code(301);
         _response.set_response_string("Moved Permanently");
         _response.set_header("Content-Type", "text/html");
         _response.set_header("Content-Length", "0");
+
+        const std::string& location_name = _request.config()->redirect.second;
+        if (location_name == "/")
+            _response.set_header("Location", location_name + relative_path);
+        else
+            _response.set_header("Location", location_name + "/" + relative_path);
         _request.set_status(REQ_PROCESSED);
-        // TODO Add Location header pointing to the right resource
         return;
     }
-
-    std::string relative_path = _request.target().substr(_request.config()->name.length());
-    if (!relative_path.empty() &&  // If we still have a slash at the beginning, remove it
-        relative_path[0] == '/')
-        relative_path.erase(0, relative_path.find_first_not_of('/'));
 
     switch (_request.method()) {
         case GET:
